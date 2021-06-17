8a80db0dff8fdafaf65f55930bb14defd287638d
elyam_sepha <77883937+elyamsepha@users.noreply.github.com>
2021-06-16 22:54:53 -0300

    Update hentai.py

--- hentai.py
+++ hentai.py
@@ -10,57 +10,59 @@ codigo = input("código do hentai: ")
 
 URL = f"https://nhentai.to/g/{codigo}"
 
-try:
-    response = requests.get(URL, headers=header)
-except:
-    print("código incorreto!")
-    input("aperte enter para fechar...")
-    exit
 
-content = response.content
-site = BeautifulSoup(content, 'html.parser')
+response = requests.get(URL, headers=header)
+
+if response.status_code == 200:
+    content = response.content
+    site = BeautifulSoup(content, 'html.parser')
 
 
-lista = []
+    lista = []
 
-titulo = site.find('div', attrs={'id': "info"})
-n = 0
-for titu in titulo:
-    if n == 1:
-        for t in titu:
-            titulo = t
-        break
-    else:
-        n = n + 1
+    titulo = site.find('div', attrs={'id': "info"})
+    n = 0
+    for titu in titulo:
+        if n == 1:
+            for t in titu:
+                titulo = t
+            break
+        else:
+            n = n + 1
 
-imagens = site.find_all('a', attrs={'class': "gallerythumb"})
+    imagens = site.find_all('a', attrs={'class': "gallerythumb"})
 
-for imagem in imagens:
-    lista.append("https://nhentai.to"+imagem.get('href'))
-os.system("cls")
-print(f"-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nTítulo: {titulo}\n\nquantidade de capítulos: {len(lista)}\n\n[1] - baixar hentai\n[2] - sair\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n")
-escolha = input("escolha: ")
-print("\n\n")
-if escolha == "1" or escolha == 1:
-    num = 1
-    for item in lista:
-        response = requests.get(item, headers=header)
-
-        content = response.content
-        site = BeautifulSoup(content, 'html.parser')
-
-        imagem = site.find('img', attrs={'class': "fit-horizontal"})
-        photo = (imagem.get('src'))
-
-        response = requests.get(photo, headers=header)
-
-
-        with open(f"capítulo {num}.png", 'wb') as f:
-            f.write(response.content)
-        print(f"página número {num} baixado!")
-        num = num + 1
+    for imagem in imagens:
+        lista.append("https://nhentai.to"+imagem.get('href'))
     os.system("cls")
-    input("todas as páginas do hentai foram baixadas!\npressione enter para fechar")
+    print(f"-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nTítulo: {titulo}\n\nquantidade de capítulos: {len(lista)}\n\n[1] - baixar hentai\n[2] - sair\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n")
+    escolha = input("escolha: ")
+    print("\n\n")
+    if escolha == "1" or escolha == 1:
+        num = 1
+        for item in lista:
+            response = requests.get(item, headers=header)
 
-if escolha == "2" or escolha == 2:
+            content = response.content
+            site = BeautifulSoup(content, 'html.parser')
+
+            imagem = site.find('img', attrs={'class': "fit-horizontal"})
+            photo = (imagem.get('src'))
+
+            response = requests.get(photo, headers=header)
+
+
+            with open(f"capítulo {num}.png", 'wb') as f:
+                f.write(response.content)
+            print(f"página número {num} baixado!")
+            num = num + 1
+        os.system("cls")
+        input("todas as páginas do hentai foram baixadas!\npressione enter para fechar")
+
+    if escolha == "2" or escolha == 2:
+        exit
+
+if response.status_code >= 400:
+    print("código de hentai incorreto!")
+    input("pressione enter para fechar...")
     exit
